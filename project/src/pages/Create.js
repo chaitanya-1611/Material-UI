cdb3233b3b62df8e2e2e9c9a7a9790d8d56d5791
Chaitanya <sdcbbarewar15@gmail.com>
2024-12-04 19:25:23 +0530

    Typography added

--- project/src/pages/Create.js
+++ project/src/pages/Create.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import Typography from "@mui/material/Typography";
 
 const create = () => {
   return (
     <div>
-        <h1>Hello, World!</h1>
-      
+      <Typography
+        variant="h6"
+        color="warning"
+        component="h2"
+        align="center"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
     </div>
-  )
-}
+  );
+};
 
-export default create
+export default create;
